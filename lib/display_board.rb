1736b07430248643d00651a2084f514337791713
Brittany Tinnin <bntinnin7@gmail.com>
2018-04-27 02:24:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-board = ["O","O","O","O","O","O","O","O","O"]
+board = ["O","O","O","O","O","O","O","O"]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
