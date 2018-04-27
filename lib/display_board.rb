51d8a55ba2bbe75c54474d8a857f440686843894
Brittany Tinnin <bntinnin7@gmail.com>
2018-04-27 01:47:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 board = [" "," "," "," "," "," "," "," "," "]
 rows = ["   |   |   \n", "-----------\n","   |   |   \n", "-----------\n","   |   |   \n"]
 def display_board(board)
-  prints board
+  print board
 end
 # Define display_board that accepts a board and prints
 # out the current state.
