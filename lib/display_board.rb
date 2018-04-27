5bb0b1d2ccdce61b11a820d53a351431682487b1
Brittany Tinnin <bntinnin7@gmail.com>
2018-04-27 01:44:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
-board = [" "," "," "," "," "," "," "," "," "]
-def display_board(board)
-  puts board
+rows = ["   |   |   \n", "-----------\n","   |   |   \n", "-----------\n","   |   |   \n"]
+def display_board(rows)
+  prints board
 end
 # Define display_board that accepts a board and prints
 # out the current state.
